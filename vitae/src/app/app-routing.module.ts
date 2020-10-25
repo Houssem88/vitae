5108dfb668e209279490cc3725e0d1730850ecdf
Houssem <hour2d2@gmail.com>
2020-10-25 20:57:33 +0100

    added routes and components

--- vitae/src/app/app-routing.module.ts
+++ vitae/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BioComponent } from './routes/bio/bio.component';
+import { CvComponent } from './routes/cv/cv.component';
+import { HomeComponent } from './routes/home/home.component';
+import { LinksComponent } from './routes/links/links.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'bio', component: BioComponent },
+  { path: 'cv', component: CvComponent },
+  { path: 'links', component: LinksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- vitae/src/app/app.module.ts
+++ vitae/src/app/app.module.ts
@@ -3,19 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './layout/main/main.component';
+import { LayoutModule } from './layout/layout.module';
+import { BioComponent } from './routes/bio/bio.component';
+import { CvComponent } from './routes/cv/cv.component';
+import { LinksComponent } from './routes/links/links.component';
+import { HomeComponent } from './routes/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BioComponent, CvComponent, LinksComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [MainComponent]
 })
-export class AppModule { }
+export class AppModule {}
